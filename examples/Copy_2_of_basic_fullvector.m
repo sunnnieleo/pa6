42441e8857940c89cd4c4124254d2dfb3f0f12eb
Sonya Stuhec-Leonard <sonya.stuhec.leonard@gmail.com>
2018-03-01 18:03:01 -0500

    pa 6 submittion

---
+++ examples/Copy_2_of_basic_fullvector.m
@@ -0,0 +1,87 @@
+% This example shows how to calculate and plot both the
+% fundamental TE and TM eigenmodes of an example 3-layer ridge
+% waveguide using the full-vector eigenmode solver.  
+
+% Refractive indices:
+n1 = 3.34;          % Lower cladding
+n2 = 3.44;          % Core
+n3 = 1.00;          % Upper cladding (air)
+% This example shows how to calculate and plot both the
+% fundamental TE and TM eigenmodes of an example 3-layer ridge
+% waveguide using the full-vector eigenmode solver.  
+
+% Refractive indices:
+n1 = 3.34;          % Lower cladding
+n2 = 3.44;          % Core
+n3 = 1.00;          % Upper cladding (air)
+
+% Layer heights:
+h1 = 2.0;           % Lower cladding
+h2 = 1.3;           % Core thickness
+h3 = 0.5;           % Upper cladding
+
+% Horizontal dimensions:
+rh = 1.1;           % Ridge height
+rw = 1.0;           % Ridge half-width
+% side = 1.5;         % Space on side
+
+% Grid size:
+dx = 0.0125;        % grid size (horizontal)
+dy = 0.0125;        % grid size (vertical)
+
+lambda = 1.55;      % vacuum wavelength
+nmodes = 10;         % number of modes to compute
+
+for side=3.305:0.0150:3.44
+    [x,y,xc,yc,nx,ny,eps,edges] = waveguidemesh([n1,n2,n3],[h1,h2,h3], ...
+        rh,rw,side,dx,dy);
+    
+    % First consider the fundamental TE mode:
+    
+    [Hx,Hy,neff] = wgmodes(lambda,n2,nmodes,dx,dy,eps,'000A');
+    
+    fprintf(1,'neff = %.6f\n',neff);
+    
+    for i=1:10
+        figure
+        subplot(121);
+        contourmode(x,y,Hx(:, :, i));
+        title('Hx (TE mode)'); xlabel('x'); ylabel('y');
+        for v = edges, line(v{:}); end
+        
+        subplot(122);
+        contourmode(x,y,Hy(:, :, i));
+        title('Hy (TE mode)'); xlabel('x'); ylabel('y');
+        for v = edges, line(v{:}); end
+    end
+    
+    % % Next consider the fundamental TM mode
+    % % (same calculation, but with opposite symmetry)
+    %
+    % [Hx,Hy,neff] = wgmodes(lambda,n2,nmodes,dx,dy,eps,'000S');
+    %
+    %
+    % fprintf(1,'neff = %.6f\n',neff);
+    %
+    % for i=1:10
+    %     figure(2);
+    %     subplot(121);
+    %     contourmode(x,y,Hx(:, :, i));
+    %     title('Hx (TM mode)'); xlabel('x'); ylabel('y');
+    %     for v = edges, line(v{:}); end
+    %
+    %     subplot(122);
+    %     contourmode(x,y,Hy(:, :, i));
+    %     title('Hy (TM mode)'); xlabel('x'); ylabel('y');
+    %     for v = edges, line(v{:}); end
+    %
+    % end
+end
+
+
+side_vec = 3.305:0.0150:3.44;
+figure
+plot(side_vec, neff)
+title('Neff vs rw')
+xlabel('side')
+ylabel('neff')
